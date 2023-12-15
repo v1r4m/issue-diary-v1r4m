2cd28d5e26094365f2e889b127e1db4cf33e2588
v1r4m <choyoung7789@gmail.com>
2023-12-15 21:29:26 +0900

    calender...

--- diary/src/app/test/page.tsx
+++ diary/src/app/test/page.tsx
@@ -1,18 +1,77 @@
-import { fetchData } from '../../utils/fetchData';
-import React, { useEffect } from 'react';
+'use client';
+import React, { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
-export default async function Home(){
+const CalendarApp: React.FC = () => {
 
-    const response = await fetch('https://api.github.com/users/v1r4m');
-    const data = await response.json();
-    console.log(data);
-  
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => handleSwipeLeft(),
+    onSwipedRight: () => handleSwipeRight(),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  });
+
+  const handleSwipeLeft = () => {
+    const nextMonth = new Date(currentMonth);
+    nextMonth.setMonth(currentMonth.getMonth() + 1);
+    setCurrentMonth(nextMonth);
+  };
+
+  const handleSwipeRight = () => {
+    const prevMonth = new Date(currentMonth);
+    prevMonth.setMonth(currentMonth.getMonth() - 1);
+    setCurrentMonth(prevMonth);
+  };
+
+  const getDaysInMonth = (year: number, month: number) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  const getFirstDayOfMonth = (year: number, month: number) => {
+    return new Date(year, month, 1).getDay();
+  };
+
+  const renderCalendar = () => {
+    const year = currentMonth.getFullYear();
+    const month = currentMonth.getMonth();
+    const daysInMonth = getDaysInMonth(year, month);
+    const firstDay = getFirstDayOfMonth(year, month);
+
+    const blanks = Array(firstDay).fill(null);
+    const days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+
     return (
       <div>
         <div>
-          {data.login}
+          {['일', '월', '화', '수', '목', '금', '토'].map((day, index) => (
+            <span key={index}>{day}</span>
+          ))}
         </div>
-          {JSON.stringify(data,null,2)}
+        <div>
+          {blanks.map((_, index) => (
+            <span key={`blank-${index}`} />
+          ))}
+          {days.map((day) => (
+            <span key={day}>{day}</span>
+          ))}
+        </div>
       </div>
     );
   };
+
+  return (
+    <div>
+      <h1>캘린더 앱</h1>
+      {renderCalendar()}
+      <div>
+        <p>{currentMonth.toLocaleDateString()}</p>
+        <button onClick={handleSwipeLeft}>다음 달</button>
+        <button onClick={handleSwipeRight}>이전 달</button>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarApp;
