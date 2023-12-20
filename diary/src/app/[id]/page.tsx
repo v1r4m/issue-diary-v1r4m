d24567c4d835892e06943a2233ebce3781966c80
v1r4m <choyoung7789@gmail.com>
2023-12-20 22:38:04 +0900

    delete dataslot

--- diary/src/app/[id]/page.tsx
+++ diary/src/app/[id]/page.tsx
@@ -187,11 +187,11 @@ const CalendarApp: React.FC = () => {
         {renderCalendar()}
       </div>
       <div className="h-1/10 grid grid-cols-2 max-w-s mx-auto gap-2">
-        <button className="py-2 text-center" onClick={handleSwipeRight}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" dataSlot="icon" className="w-6 h-6">
+        <button className="py-2 text-center" onClick={handleSwipeRight}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
 </svg>
 </button>
-        <button className="py-2 text-center" onClick={handleSwipeLeft}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" dataSlot="icon" className="w-6 h-6">
+        <button className="py-2 text-center" onClick={handleSwipeLeft}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
   <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
 </svg>
 </button>
